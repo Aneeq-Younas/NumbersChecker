ace19aaee2189f8fa081cc4bfb461e9227ad9e08
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-23 00:08:04 -0700

    Add files via upload

---
+++ anynum.py
@@ -0,0 +1,12 @@
+number = int(input("Enter any integer positive or negative number: "))
+if number > 0:
+    print("This is a positive number ")
+
+elif number == 0:
+    print("Its a zero and positive number ")
+
+elif number <= -1:
+    print("This is negative number ")
+
+else:
+    print(input(" Error  "))
